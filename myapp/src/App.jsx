1c3459cbdcd609aa93914ca784982cfe5c972e68
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-10-17 12:34:31 +0100

    Co-authored-by: Tess Phillips <tess-phillips@users.noreply.github.com>

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -7,6 +7,7 @@ import TriviaApp from './components/TriviaApp'
 function App() {
 return (
   <>
+  <h1>Trivia Questions</h1>
   <TriviaApp/>
   </>
 )
--- myapp/src/components/TriviaApp.jsx
+++ myapp/src/components/TriviaApp.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
 import { getTriviaQuestions } from "../services/api";
+import { shuffleArray } from "../utils/shuffleArray";
 
 function TriviaApp() {
   const [questions, setQuestions] = useState([]);
-    // console.log(questions)
+  const [userAnswers, setUserAnswers] = useState([]);
+
   useEffect(() => {
     async function fetchTrivia() {
       try {
         const data = await getTriviaQuestions()
-        setQuestions(data.results);
+        const shuffledQuestions = data.results.map((question) => ({
+            ...question,
+            answers: shuffleArray([...question.incorrect_answers, question.correct_answer]),
+          }));
+        setQuestions(shuffledQuestions);
+        console.log(data)
       } catch (error) {
         console.error(error);
       }
@@ -16,14 +23,38 @@ function TriviaApp() {
     fetchTrivia();
   }, []);
 
+  
+  const handleAnswerChange = (questionIndex, selectedAnswer) => {
+    // Create a copy of the userAnswers array and update the selected answer for the current question.
+    const updatedUserAnswers = [...userAnswers];
+    updatedUserAnswers[questionIndex] = selectedAnswer;
+    setUserAnswers(updatedUserAnswers);
+  };
+
+
   return (
     <div>
-      <h1>Trivia Questions</h1>
       <ul>
         {questions.map(question => (
           <li key={question.question}>
             <h3>Question: {question.question}</h3>
-            <>Incorrect Answers: {question.incorrect_answers.map(item => item + " ")}</>
+            <div>
+              {
+              question.answers.map((answer, index) => (
+                <label key={answer}>
+                  <input
+                    type="radio"
+                    name={`question${question.question}`}
+                    value={answer}
+                    onChange={(e) =>
+                      handleAnswerChange(index, e.target.value)
+                    }
+                  />
+                  {answer}
+                </label>
+           
+              ))}
+            </div>
             <p>Correct Answer: {question.correct_answer}</p>
           </li>
         ))}
